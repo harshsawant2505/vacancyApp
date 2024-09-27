0ae660c8ecdb70083a176641187ba23f8457b14a
Anirudha <aniruhaj.9876@gmail.com>
2024-09-27 22:50:36 +0530

    Req commit 22:50 27 SEP 2024

--- lib/mobile/Pages/map page/mappage.dart
+++ lib/mobile/Pages/map page/mappage.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
 
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final List<LatLng> locations = [
+    const LatLng(37.7749, -122.4194), // San Francisco
+    const LatLng(34.0522, -118.2437), // Los Angeles
+    const LatLng(40.7128, -74.0060), // New York
+    // Add more locations as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        
+      ],
+    );
+  }
+}
